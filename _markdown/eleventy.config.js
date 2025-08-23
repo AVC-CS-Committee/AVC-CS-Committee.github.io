c15f0a1470e828dcae39f9b30cd5867fd57c5079
River Seeber <riverseeber12@gmail.com>
2025-08-22 20:20:09 -0700

    make CSS mobile friendly, some other cleanups

--- _markdown/eleventy.config.js
+++ _markdown/eleventy.config.js
@@ -1,3 +1,29 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+export default function (eleventyConfig) {
+	eleventyConfig.ignores.add("**blog/drafts/**");
+
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/blog/feed.xml",
+		collection: {
+			name: "blog", // iterate over `collections.post`
+			limit: 20,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "AVC CS Club Blog",
+			subtitle: "A blog discussing the activities going on in the Computer Science club at AVC",
+			base: "https://avc-cs-committee.github.io/blog/",
+			author: {
+				name: "CS Club",
+				email: "", // Optional
+			}
+		}
+	});
+}
+
+
 export const config = {
   dir: {
 		output: "../docs",
--- _markdown/index.md
+++ _markdown/index.md
@@ -2,10 +2,10 @@
 layout: "layout_main.html"
 title: "AVC CS Club"
 subtitle: "/home"
-edited: "August 15, 2025"
+edited: "August 22, 2025"
 ---
 
-Welcome to the AVC CS Club! During the Fall and Spring semesters, we meet **every other Friday in YH 167 from 3:30pm - 4:30pm**, right after the general STEM Club meeting. Our first meeting is on **September 5th**.
+Welcome to the AVC CS Club! We meet **every other Friday in YH 167 from 3:30pm - 4:30pm**, right after the general STEM Club meeting. Our first meeting is on **September 5th**.
 <hr>
 
 [![Discord QR Code](/images/Discord_QR_Code.png)](https://discord.gg/6XwNprAG3c)
